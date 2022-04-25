64d748df8e087618b9520fdd484d430a7cb4e68b
Joseph Lyncheski <directmusic94@gmail.com>
2022-04-25 06:22:49 -0400

    0.2.0

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Header/Source Side-By-Side
 
+Made by [Direct](https://twitter.com/directmusic) :)
+
 This extension will attempt to open a related file (myFile.cpp / myFile.h) in a tab group to the left and right. Running the command again will swap the cursor location from these two groups. This is similar to Xcode.
 
 ## Features
@@ -10,14 +12,17 @@ This extension will attempt to open a related file (myFile.cpp / myFile.h) in a 
 
 This extension contributes the following settings:
 
-* `header-source-side-by-side.do`: Open a tab group to the right or left with the related file.
+* `header-source-side-by-side.side-by-side`: Open a tab group to the right or left with the related file.
+* `header-source-side-by-side.switch_file_in_same_window`: Swap between the related file in the same group.
 
 ## Release Notes
 
-### 0.0.1
+#### 0.2.0
+- Renamed `do` to `side-by-side`
+- Fixed glitchiness when swapping between two groups.
+- Added `switch_file_in_same_window` as an alternative to the C/C++ extension's Header/Implementation swap.
 
-Initial release.
+**0.1.0** Switched to Node's fs for speed and the ability to filter easier.
 
-### 0.1.0
+**0.0.1** Initial release.
 
-Switched to Node's fs for speed and the ability to filter easier.
--- extension.js
+++ extension.js
@@ -12,7 +12,36 @@ function get_file_and_folder_name(file) {
  */
 function activate(context) {
 	console.log('Header / Source Side-By-Side is installed and active!');
-	let disposable = vscode.commands.registerCommand('header-source-side-by-side.do', function () {
+
+	let hs_switch = vscode.commands.registerCommand('header-source-side-by-side.switch_file_in_same_window', function () {
+		let active_doc = vscode.window.activeTextEditor;
+		let ff = get_file_and_folder_name(active_doc.document.fileName);
+		fs.readdir(ff.dir, function (err, files) {
+			if (err) {
+				return console.log('Unable to scan directory: ' + err);
+			}
+			files.forEach(function (file) {
+				let split = file.split(".");
+				if (split.length < 1)
+					return;
+
+				let name = split[0];
+				let extension = split[split.length - 1];
+
+				if (name == ff.name && file != ff.name_w_extension) {
+					console.log("true " + file + " ff " + ff.name_w_extension);
+
+					// Open Editor
+					vscode.workspace.openTextDocument(ff.dir + "/" + file).then(file => {
+						vscode.window.showTextDocument(file);
+					})
+				}
+			});
+
+		});
+	});
+
+	let side_by_side = vscode.commands.registerCommand('header-source-side-by-side.side-by-side', function () {
 		let current_open_docs = vscode.window.visibleTextEditors;
 		let active_doc = vscode.window.activeTextEditor;
 		let ff = get_file_and_folder_name(active_doc.document.fileName);
@@ -27,8 +56,6 @@ function activate(context) {
 				return console.log('Unable to scan directory: ' + err);
 			}
 
-			let winners = [];
-
 			files.forEach(function (file) {
 				let split = file.split(".");
 				if (split.length < 1)
@@ -36,8 +63,31 @@ function activate(context) {
 
 				let name = split[0];
 				let extension = split[split.length - 1];
+
 				if (name == ff.name && file != ff.name_w_extension) {
 					console.log("true " + file + " ff " + ff.name_w_extension);
+					let open_columns = vscode.window.visibleTextEditors;
+					let found_visible_column_with_winning_file = false;
+
+					for (let i = 0; i < open_columns.length; i++) {
+						const column = open_columns[i];
+
+						if (column.document.fileName == ff.dir + "/" + file) {
+							// skip this one because it's our current one
+							if (i == active_column)
+								continue;
+							// we found a file in editor id (i) switch to (i) editor.
+							if (i == 1)
+								vscode.commands.executeCommand('workbench.action.focusFirstEditorGroup')
+							else if (i == 2)
+								vscode.commands.executeCommand('workbench.action.focusSecondEditorGroup')
+							found_visible_column_with_winning_file = true;
+						}
+					}
+
+					// skip opening a new eidtor if we found one
+					if (found_visible_column_with_winning_file)
+						return;
 
 					// Open Editor
 					vscode.workspace.openTextDocument(ff.dir + "/" + file).then(file => {
@@ -55,7 +105,8 @@ function activate(context) {
 		});
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(side_by_side);
+	context.subscriptions.push(hs_switch);
 }
 
 // this method is called when your extension is deactivated
